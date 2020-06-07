98cfade28c250c3cf6c62284a9eaf1ddff92048c
Nels0n-Ticas <nticashenriquez@gmail.com>
2020-06-07 12:07:07 -0600

    elemento eliminar agregado

--- src/java/modelo/Persona.java
+++ src/java/modelo/Persona.java
@@ -107,5 +107,14 @@ public void modificar(String dui, String apellidos, String nombres) {
          }
           
 }   
-
+public void eliminar(String dui) throws SQLException{
+         try {
+             
+             state = cnn.prepareStatement("delete from tb_persona where dui_persona='"+dui+"'");
+         state.executeUpdate();
+         } catch (Exception ex) {
+             System.out.println("ERROR"+ex.getMessage());
+         }
+         
+     }
 }
